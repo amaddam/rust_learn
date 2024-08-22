005aade189b2f93b971205fdc4dd04de7061d1e2
none <>
2024-08-22 10:27:43 +0800

    some concepts record and gitignore modified

--- chapter_1_hello_rust/src/main.rs
+++ chapter_1_hello_rust/src/main.rs
@@ -5,31 +5,38 @@ use std::io;
 fn main() {
     println!("Guess the number!");
 
+    //生成一个随机数
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    //打印出这个随机数
     println!("The secret number is: {}", secret_number);
 
+    //循环猜测
     loop {
         print!("Please input your guess: ");
 
+        //创建一个可变的变量来存储用户输入的值, mut的全称是mutable(可变的)
         let mut guess = String::new();
 
+        //读取用户输入的值并存储到guess变量中
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        //打印出用户输入的值
         println!("You guessed: {}", guess);
 
-        // Convert the string to a number
+        // 将用户输入的值转换为数字类型, 如果转换成功, 则将值赋给guess变量, 否则继续循环读取输入的值
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
+        //比较用户输入的值和随机数的大小
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            // If the guess is correct, we print a congratulatory message and break the loop.
+            // 如果用户输入的值和随机数相等, 则打印出You win!并退出循环
             Ordering::Equal => {
                 println!("You win!");
                 break;
--- chapter_2_concepts/src/main.rs
+++ chapter_2_concepts/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    // 变量默认是不可变的, 如果需要改变变量的值, 需要在变量名前加mut关键字
+    // let x = 5;
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
 }
