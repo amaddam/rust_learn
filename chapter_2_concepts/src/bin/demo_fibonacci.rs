39281f4523a2505c163141d1fd258459da8c039c
none <>
2024-09-27 12:25:50 +0800

    write fibonacci demo

---
+++ chapter_2_concepts/src/bin/demo_fibonacci.rs
@@ -0,0 +1,67 @@
+fn main() {
+    let mut n = String::new();
+
+    loop {
+        n.clear();
+        println!("Please enter a number.");
+        std::io::stdin()
+            .read_line(&mut n)
+            .expect("Failed to read line");
+
+        let n: i8 = match n.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a legal number");
+                continue;
+            }
+        };
+
+  
+        // 循环
+        let result = fib_loop(n);
+        println!("fib_loop({n}) = {result}");
+
+        // 递归
+        let result = fib_recursion(n);
+        println!("fib_recursion({n}) = {result}");
+
+        // 带有缓存的递归
+        let result = fib_recursion_with_cache(n);
+        println!("fib_recursion_with_cache({n}) = {result}");
+    }
+}
+
+fn fib_loop(n: i8) -> i64 {
+    if n == 0 {
+        return 1;
+    }
+    if n == 1 {
+        return 1;
+    }
+    let mut a = 1;
+    let mut b = 1;
+    let mut c = 0;
+    //从2开始, 因为0和1已经计算过了, ..=n表示包含n
+    for _ in 2..=n {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    c
+}
+
+fn fib_recursion(n: i8) -> i64 {
+    if n == 0 {
+        return 1;
+    }
+    if n == 1 {
+        return 1;
+    }
+    fib_recursion(n - 1) + fib_recursion(n - 2)
+}
+
+fn fib_recursion_with_cache(n: i8) -> i64 {
+    //todo 全局变量还没了解comp
+    println!("fib_recursion_with_cache({n})");
+    0
+}
--- chapter_2_concepts/src/bin/demo_temperature_converter.rs
+++ chapter_2_concepts/src/bin/demo_temperature_converter.rs
@@ -47,7 +47,7 @@ fn main() {
         let mut temperature: f64;
         'input_temperature: loop {
             //2. 输入度数, 可以随时输入exit退出
-            println!("Please enter the temperature: ");
+            println!("\nPlease enter the temperature: ");
             let mut temperature_input = String::new();
             std::io::stdin()
                 .read_line(&mut temperature_input)
@@ -90,15 +90,60 @@ fn main() {
         let convert_result;
 
         if CELSIUS_TO_FAHRENHEIT == mode {
-            convert_result = temperature * 1.8 + 32.0;
+            convert_result = round(temperature * 1.8 + 32.0, 2);
             println!("Celsius to Fahrenheit convert result is {convert_result}");
         } else {
-            convert_result = (temperature - 32.0) / 1.8;
+            convert_result = round((temperature - 32.0) / 1.8, 2);
             println!("Fahrenheit to Celsius convert result is {convert_result}");
         }
 
-        println!("\n")
+        println!("\n\n\n")
     }
 
     println!("Program exited.");
+}
+
+//四舍五入
+/**
+ * 1. 保留小数点后n位
+ * 2. 四舍六入五成双
+ */
+fn round(value: f64, n: i32) -> f64 {
+    let mut value = value;
+    let mut n = n;
+    if n < 0 {
+        n = 0;
+    }
+
+    let mut multiple = 1;
+
+    //_表示不使用的变量
+    for _ in 0..n {
+        multiple *= 10;
+    }
+
+    //as f64表示转换成f64类型
+    value = value * multiple as f64;
+    let value_int = value as i64;
+    let value_float = value - value_int as f64;
+
+    //如果小数部分大于0.5, 则整数部分加1
+    if value_float > 0.5 {
+        value = (value_int + 1) as f64;
+
+    //如果小数部分小于0.5, 则整数部分不变
+    } else if value_float < 0.5 {
+        value = value_int as f64;
+
+    //如果小数部分等于0.5, 则整数部分为偶数时不变, 为奇数时加1
+    } else {
+        if value_int % 2 == 0 {
+            value = value_int as f64;
+        } else {
+            value = (value_int + 1) as f64;
+        }
+    }
+
+    //as f64表示转换成f64类型
+    value / multiple as f64
 }
